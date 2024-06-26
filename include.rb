8a7392e09bca1520f01fef7f6166603c82e335ad
manh.truong <manh.truong@boardpackager.com>
2024-06-27 04:41:38 +0700

    add: ancestors chain C (inline) -> C -> M -> C2

--- include.rb
+++ include.rb
@@ -1,28 +1,56 @@
-class C
-  @@val = 10
-end
+# class C
+#   @@val = 10
+# end
 
-module B
-  @@val = 30
-end
+# module B
+#   @@val = 30
+# end
+
+# module M
+#   include B
+#   p @@val
+#   @@val = 20
+
+#   p @@val
+
+#   class << C
+#     p @@val
+#     # p ancestors
+#   end
+# end
+
+# class N < C
+#   p @@val
+
+#   class << self
+#     # p ancestors
+#   end
+# end
+
+# =====NEXT QUESTION=====
 
 module M
-  include B
-  p @@val
-  @@val = 20
-
-  p @@val
-
-  class << C
-    p @@val
-    # p ancestors
+  def foo
+    super
+    puts "M#foo"
   end
 end
 
-class N < C
-  p @@val
-
-  class << self
-    # p ancestors
+class C2
+  def foo
+    puts "C2#foo"
   end
 end
+
+class C < C2
+  def foo
+    super
+    puts "C#foo"
+  end
+  include M
+end
+
+C.new.foo
+# C2#foo
+# M#foo
+# C#foo
