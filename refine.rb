45e63c8dc8da6423aaa35de4a9c90b5d63fa0b54
manh.truong <manh.truong@boardpackager.com>
2024-06-27 04:33:54 +0700

    add: `Refinement` is a class of the *self* (current context) inside *refine* statement. It allows to import methods from other modules. The `self` keyword inside *refine* block indicate the Refinement's class object, not the class passing to refine

--- refine.rb
+++ refine.rb
@@ -1,30 +1,51 @@
-class C; end
+# class C; end
 
-module M
-	refine C do
-		def m1(value)
-			p "m1 in refine C block"
-			super value - 100
-		end
-	end
-end
+# module M
+# 	refine C do
+# 		def m1(value)
+# 			p "m1 in refine C block"
+# 			super value - 100
+# 		end
+# 	end
+# end
 
-class C
-	def m1(value)
-		puts "m1 in class C"
-		value - 100
-	end
-end
+# class C
+# 	def m1(value)
+# 		puts "m1 in class C"
+# 		value - 100
+# 	end
+# end
 
-using M
+# using M
 
-class K < C
-	def m1(value)
-		puts "m1 in class K"
-		super value - 100
-	end
-end
+# class K < C
+# 	def m1(value)
+# 		puts "m1 in class K"
+# 		super value - 100
+# 	end
+# end
 
-puts K.new.m1 400
+# puts K.new.m1 400
 # puts C.new.public_methods.include? :m1	#=> true
 # puts C.new.public_methods
+
+# =====NEXT QUESTION=====
+
+class C
+  def self.m1
+    200
+  end
+end
+
+module R
+  refine C do
+		p self.class	#=> Refinement class
+    def self.m1
+      100
+    end
+  end
+end
+
+using R
+
+puts C.m1	#=> 200
