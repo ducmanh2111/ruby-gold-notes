f3c20a5938fb14c4fd40f76f868cc2bcdcb2b929
manh.truong <manh.truong@boardpackager.com>
2024-06-27 05:25:03 +0700

    add: sort using <=> (spaceship) operator

---
+++ sort.rb
@@ -0,0 +1,26 @@
+class Company
+  attr_reader :id
+  attr_accessor :name
+  def initialize id, name
+    @id = id
+    @name = name
+  end
+  def to_s
+    "#{id}:#{name}"
+  end
+  def <=> other
+    # other.id <=> self.id
+    self.id <=> other.id
+  end
+end
+
+companies = []
+companies << Company.new(2, 'Liberyfish')
+companies << Company.new(3, 'Freefish')
+companies << Company.new(1, 'Freedomfish')
+
+companies.sort!
+
+companies.each do |e|
+  puts e
+end
