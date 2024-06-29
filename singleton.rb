e28e0ebc4d71efcab1cb3ab10e7bb7da49065499
manh.truong <manh.truong@boardpackager.com>
2024-06-30 00:44:13 +0700

    add: Object#singleton_methods

---
+++ singleton.rb
@@ -0,0 +1,37 @@
+class N
+  def self.foo
+    p "class method foo in class N invoked!"
+  end
+
+  class << self
+    def foo
+      p "instance method foo in singleton class of N's instance invoked!"
+    end
+  end
+end
+
+class M < N
+  def self.foo
+    p "class method foo in class M invoked!"
+  end
+
+  class << self
+    def foo
+      super
+      p "instance method foo in singleton class of M's instance invoked!"
+    end
+  end
+end
+
+# M.foo
+# "instance method foo in singleton class of N's instance invoked!"
+# "instance method foo in singleton class of M's instance invoked!"
+# p M.singleton_class.ancestors
+# [#<Class:M>, #<Class:N>, #<Class:Object>, #<Class:BasicObject>, Class, Module, Object, Kernel, BasicObject]
+# p M.ancestors
+# [M, N, Object, Kernel, BasicObject]
+# p M.new.singleton_methods(false)
+#=> []
+
+# p M.singleton_methods
+#=> [:foo]
