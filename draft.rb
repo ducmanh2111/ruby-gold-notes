9091030ee3da233aff5fef36a176fa7c8931040d
manh.truong <manh.truong@boardpackager.com>
2024-06-30 03:37:38 +0700

    add: question about define Array#succ_each

--- draft.rb
+++ draft.rb
@@ -1,7 +1,35 @@
-p Class.method_defined? :new #=> ture
-p String.method_defined? :new #=> false
-p Class.singleton_class.method_defined? :new #=> ture
-p String.singleton_class.method_defined? :new #=> ture
+# p Class.method_defined? :new #=> ture
+# p String.method_defined? :new #=> false
+# p Class.singleton_class.method_defined? :new #=> ture
+# p String.singleton_class.method_defined? :new #=> ture
 
 # =====NEXT QUESTION=====
 
+# def hoge(*args, &block)
+#   block.call(args)
+# end
+
+# hoge(1,2,3,4) do |*args|
+#   p args.length < 0 ? "hello" : args
+# end
+
+# =====NEXT QUESTION=====
+class Array
+  def succ_each(step = 1)
+    return enum_for(:succ_each, step) unless block_given?
+
+    each do |int|
+      yield int + step
+    end
+  end
+end
+
+p [98, 99, 100].succ_each(2).map {|succ_chr| succ_chr.chr}  # ["d", "e", "f"]
+
+[101, 102, 103].succ_each(5) do |succ_chr|
+  p succ_chr.chr
+end
+
+# "j"
+# "k"
+# "l"
