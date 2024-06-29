5e41ff9db79b3c31dca699ad3539a61c2f9764d9
manh.truong <manh.truong@boardpackager.com>
2024-06-30 01:10:55 +0700

    add: class variable in singleton class

--- singleton.rb
+++ singleton.rb
@@ -1,27 +1,27 @@
-class N
-  def self.foo
-    p "class method foo in class N invoked!"
-  end
+# class N
+#   def self.foo
+#     p "class method foo in class N invoked!"
+#   end
 
-  class << self
-    def foo
-      p "instance method foo in singleton class of N's instance invoked!"
-    end
-  end
-end
+#   class << self
+#     def foo
+#       p "instance method foo in singleton class of N's instance invoked!"
+#     end
+#   end
+# end
 
-class M < N
-  def self.foo
-    p "class method foo in class M invoked!"
-  end
+# class M < N
+#   def self.foo
+#     p "class method foo in class M invoked!"
+#   end
 
-  class << self
-    def foo
-      super
-      p "instance method foo in singleton class of M's instance invoked!"
-    end
-  end
-end
+#   class << self
+#     def foo
+#       super
+#       p "instance method foo in singleton class of M's instance invoked!"
+#     end
+#   end
+# end
 
 # M.foo
 # "instance method foo in singleton class of N's instance invoked!"
@@ -35,3 +35,21 @@ end
 
 # p M.singleton_methods
 #=> [:foo]
+
+# =====NEXT QUESTION=====
+
+# class C
+#   @@val = 5
+#   class << self
+#     @@val = 10
+#   end
+
+#   def foo
+#     @@val += 20
+#   end
+# end
+
+# c = C.new
+# c.foo
+# p C.class_variable_get(:@@val)
+# #=> 30
