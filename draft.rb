68596a65adcf46371f8956481a9e77494b34d2d5
manh.truong <manh.truong@boardpackager.com>
2024-06-30 03:31:16 +0700

    add: draft.rb file to save some interesting question

---
+++ draft.rb
@@ -0,0 +1,7 @@
+p Class.method_defined? :new #=> ture
+p String.method_defined? :new #=> false
+p Class.singleton_class.method_defined? :new #=> ture
+p String.singleton_class.method_defined? :new #=> ture
+
+# =====NEXT QUESTION=====
+
