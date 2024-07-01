4ce2da355e19e71e2eb36062d29f96fcaf312ad4
manh.truong <manh.truong@boardpackager.com>
2024-07-02 00:16:14 +0700

    example: alias and Module#alias_method

--- alias.rb
+++ alias.rb
@@ -1,9 +1,26 @@
-class String
-  alias :hoge :reverse
-  # alias hoge reverse
+# class String
+#   alias :hoge :reverse
+#   # alias hoge reverse
 
-  # alias_method :hoge, :reverse
-  # alias_method "hoge", "reverse"
+#   # alias_method :hoge, :reverse
+#   # alias_method "hoge", "reverse"
+# end
+
+# p "12345".hoge
+
+# =====NEXT QUESTION=====
+
+def method
+  puts "Hello World"
 end
 
-p "12345".hoge
+alias old_method method
+
+def method
+  old_method
+  puts "Hello, Ruby World"
+end
+method
+
+# => Hello World
+# => Hello, Ruby World
