583fbdbe8696d9b1b5111962f9c86a5e2edd0801
manh.truong <manh.truong@boardpackager.com>
2024-07-03 06:39:52 +0700

    add another example in const hierachy

--- constant.rb
+++ constant.rb
@@ -148,3 +148,33 @@
 
 # =====NEXT QUESTION=====
 
+class Base
+  CONST = "Hello, world"
+end
+
+class C < Base
+end
+
+module P
+  CONST = "Good, night"
+end
+
+class Base
+  prepend P
+end
+
+module M
+  class C
+    CONST = "Good, evening"
+  end
+end
+
+module M
+  class ::C
+    def greet
+      CONST
+    end
+  end
+end
+
+p C.new.greet # "Good, night"
