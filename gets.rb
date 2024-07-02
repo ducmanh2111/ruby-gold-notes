98b83912eb4c4cc116b82d0a8f11b01bbc39e849
manh.truong <manh.truong@boardpackager.com>
2024-07-03 06:32:23 +0700

    add: more example for DATA and __END__

--- gets.rb
+++ gets.rb
@@ -1,7 +1,19 @@
-while DATA.gets
-  puts $_ if $_ =~ /Ruby/
+# while DATA.gets
+#   puts $_ if $_ =~ /Ruby/
+# end
+# __END__
+# Java programming
+# Ruby programming
+# C programming
+
+# =====NEXT QUESTION=====
+
+while not DATA.eof?
+  print DATA.read 1
 end
+
 __END__
-Java programming
-Ruby programming
-C programming
+1
+2
+3
+4
