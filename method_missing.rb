314634b4cca131b2c6e899c88b9a2a508e3e8018
manh.truong <manh.truong@boardpackager.com>
2024-07-03 08:16:35 +0700

    add more example to method_missing (class method) and method_missing (instance method)

--- method_missing.rb
+++ method_missing.rb
@@ -1,17 +1,32 @@
-class Class
-  def method_missing(id, *args)
-    puts "Class#method_missing"
+# class Class
+#   def method_missing(id, *args)
+#     puts "Class#method_missing"
+#   end
+# end
+# class A
+#   def method_missing(id, *args)
+#     puts "A#method_missing"
+#   end
+# end
+# class B < A
+#   def method_missing(id, *args)
+#     puts "B#method_missing"
+#   end
+# end
+
+# B.dummy_method  # Class#method_missing
+
+# =====NEXT QUESTION=====
+
+class C
+  def method_missing method_name
+    puts method_name
   end
-end
-class A
-  def method_missing(id, *args)
-    puts "A#method_missing"
-  end
-end
-class B < A
-  def method_missing(id, *args)
-    puts "B#method_missing"
+
+  def self.method_missing method_name
+    p method_name
   end
 end
 
-B.dummy_method  # Class#method_missing
+C.new.b # b
+C.b # :b
