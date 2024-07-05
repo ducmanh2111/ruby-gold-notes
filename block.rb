387d7fdcac0e55cad9410a3c4592a186bd410f4a
manh.truong <manh.truong@boardpackager.com>
2024-07-06 02:08:44 +0700

    add: example about return and next in block code

--- block.rb
+++ block.rb
@@ -18,16 +18,44 @@
 
 # =====NEXT QUESTION=====
 
-def m1(*)
-  str = yield if block_given?
-  p "m1 #{str}"
+# def m1(*)
+#   str = yield if block_given?
+#   p "m1 #{str}"
+# end
+
+# def m2(*)
+#   str = yield if block_given?
+#   p "m2 #{str}"
+# end
+
+# m1 m2 {
+#   "hello"
+# }
+
+# =====NEXT QUESTION=====
+
+# def foo
+#   [1,2,3].each do |i|
+#     # return if i == 2  # this will return from enclosing method, you will only see value: 1
+#     next if i == 2  # this will make result: 1  3
+
+#     puts i
+#   end
+# end
+# foo
+
+# =====NEXT QUESTION=====
+
+def foo
+  yield(5) if block_given?
+
+  p "Done!"
 end
 
-def m2(*)
-  str = yield if block_given?
-  p "m2 #{str}"
-end
+foo do |max|
+  i = rand
+  return puts(i * 10) if (i * 10) > 5
 
-m1 m2 {
-  "hello"
-}
+  puts "now we will check for next clause"
+  next if (i * 10) < 3
+end
