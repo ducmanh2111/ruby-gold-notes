4b35cc74ff053e48dcce2c522480a34d2296dbda
manh.truong <manh.truong@boardpackager.com>
2024-07-06 11:33:44 +0700

    reorganize: const_missing examples

--- const_missing.rb
+++ const_missing.rb
@@ -1,12 +1,37 @@
-class C
-  def self.const_missing(const)
-    puts "#{const} is undefined-setting it to 1."
-    const_set(const, 1)
-  end
-end
+# class C
+#   def self.const_missing(const)
+#     puts "#{const} is undefined-setting it to 1."
+#     const_set(const, 1)
+#   end
+# end
 
-puts C::A
-puts C::A
+# puts C::A
+# puts C::A
 # A is undefined-setting it to 1.
 # 1 <= return value from const_set method
 # 1 <= second call of C::A
+
+# =====NEXT QUESTION=====
+
+class Foo
+	def self.const_missing a
+		p "#{a} is undefined in class #{self}"
+	end
+
+	def const_missing a
+		p "#{a}"
+	end
+end
+
+Foo::B
+# "B is undefined in class Foo"
+
+# =====NEXT QUESTION=====
+# class Object
+# 	def Object.const_missing a
+# 		p "Constant #{a} is undefined in class #{self}"
+# 	end
+# end
+
+# B
+# "Constant B is undefined in class Object"
